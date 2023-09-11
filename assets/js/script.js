41cc75443e60380234f506fc6262167a0d13a77e
juliachelsie <>
2023-09-11 13:51:53 +0000

    Add function that runs when game is over

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ const game = () => {
     let moves = 0;
 
     const playGame = () => {
-        const rockButton = document.querySelector('rock');
-        const paperButton = document.querySelector('paper');
-        const scissorButton = document.querySelector('scissors');
+        const rockButton = document.querySelector('.rock');
+        const paperButton = document.querySelector('.paper');
+        const scissorButton = document.querySelector('.scissors');
         const pOptions = [rockButton, paperButton, scissorButton];
         const cOptions = ['rock', 'paper', 'scissor'];
 
@@ -41,7 +41,7 @@ const winner = (player, computer) => {
         result.textContent = "It's a Tie!"
     }
     else if (player == 'rock'){
-        if (computer == 'paper')
+        if (computer == 'paper'){
         result.textContent = 'Sorry, the computer won!';
         computerScore++;
         cScoreboard.textContent = computerScore;
@@ -50,4 +50,65 @@ const winner = (player, computer) => {
         playerScore++;
         pScoreBoard.textContent = playerScore;
     }
+} 
+    else if(player == 'scissors'){
+    if(computer == 'rock'){
+        result.textContent = 'Sorry, the computer won!';
+        computerScore++;
+        cScoreboard.textContent = computerScore;
+    } else {
+        result.textContent = 'You won!';
+        playerScore++;
+        pScoreBoard.textContent = playerScore;
+    }
 }
+else if(player == 'paper'){
+    if(computer == 'scissors'){
+        result.textContent = 'Sorry, the computer won!';
+        computerScore++;
+        cScoreboard.textContent = computerScore;
+    } else {
+        result.textContent = 'You won!';
+        playerScore++;
+        pScoreBoard.textContent = playerScore;
+    }
+} 
+}
+
+/* Function that runs when the game is over */
+const gameOver = (pOptions, movesLeft) => {
+    const move = document.querySelector('.makeMove');
+    const result = document.querySelector('.result');
+    const reload = document.querySelector('.reload');
+
+    pOptions.forEach(option => {
+        option.style.display = 'none';
+    })
+
+    move.innerText = 'Game Over!'
+    movesLeft.style.display = 'none';
+
+    if(playerScore > computerScore){
+        result.style.fontSize = '3rem';
+        result.innerText = 'yay You Won The Game! :D';
+        result.style.color = '#344B48';
+    } else if(playerScore < computerScore){
+        result.style.fontSize = '3rem';
+        result.innerText = 'Sorry, you lost this time. Try again!';
+        result.style.color = '#344B48';
+    } else{
+        result.style.fontSize = '3rem';
+        result.innerText = "It's a Tie!";
+        result.style.color = '#344B48';
+    }
+    reload.innerText = 'Try Again!';
+    reload.style.display = 'flex';
+    reload.addEventListener('click',() => {
+        window.location.reload();
+    })
+    
+   
+
+}
+
+
